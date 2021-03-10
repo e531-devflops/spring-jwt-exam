95b8a5400979d006d1c49af3008f1defc2e4bac1
e531-devflops <e531.devflops@gmail.com>
2021-03-10 11:44:40 +0900

    initial commit

---
+++ src/main/java/kr/daoko/exam/SpringJwtExamApplication.java
@@ -0,0 +1,13 @@
+package kr.daoko.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJwtExamApplication.class, args);
+    }
+
+}
